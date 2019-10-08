5be80aeafdd63294b2b8cdf485bd2aa244df41b6
mariia.gnutova <m.gnutova@myphotobook.de>
2019-10-08 09:57:37 +0200

    more cookie

--- script/language.js
+++ script/language.js
@@ -1,8 +1,25 @@
+ function getlangCookie(){
+	var languageCookie = document.cookie;
+	if(languageCookie.length == 0){
+		setLangCookie("en");
+		document.cookie = "path=/; expires=Tue, 19 Jan 2038 03:14:07 GMT";
+	} else if (languageCookie.includes("de")){
+		window.onload= function(){changeLanguage("de")};
+	}
+}
 
+function setLangCookie(langValue){
+	var a = document.cookie = "language="+langValue;
+	console.log(a);
+}
+
+
+window.onload = function(){
 var enLanguage = document.getElementById("en");
 enLanguage.addEventListener("click", function(){changeLanguage(enLanguage.id)});
- var deLanguage = document.getElementById("de");
+var deLanguage = document.getElementById("de");
  deLanguage.addEventListener("click", function(){changeLanguage(deLanguage.id)});
+}
 
 function changeLanguage(elementId){
   var elementForChange = document.getElementById(elementId);
@@ -15,6 +32,7 @@ function changeLanguage(elementId){
     document.getElementById("certificates").innerHTML = "Zertifikate";
     workExperience[0].classList.add("deactivated");
     workExperience[1].classList.remove("deactivated");
+	setLangCookie("de");
   }
   else{
     document.getElementById("de").classList.remove("on");
@@ -22,5 +40,6 @@ function changeLanguage(elementId){
     document.getElementById("certificates").innerHTML = "Certificates";
     workExperience[1].classList.add("deactivated");
     workExperience[0].classList.remove("deactivated");
+	setLangCookie("en");
   }
 }
